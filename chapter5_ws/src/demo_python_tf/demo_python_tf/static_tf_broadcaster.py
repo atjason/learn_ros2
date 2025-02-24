bc681088fc961f4f14834e58837879cc6c2b7c81
Jason <quietjosen@example.com>
2025-02-24 16:34:48 +0800

    Add StaticTFBoradcaster.

---
+++ chapter5_ws/src/demo_python_tf/demo_python_tf/static_tf_broadcaster.py
@@ -0,0 +1,36 @@
+import math
+import rclpy
+from rclpy.node import Node
+from tf2_ros import StaticTransformBoradcaster
+from geometry_msgs.msg import TransformStamped
+from tr_transformations import quaternion_from_euler
+
+class StaticTFBoradcaster(Node):
+  def __init__(self):
+    super().__init__('static_tf2_broadcaster')
+    self.static_broadcaster = StaticTransformBoradcaster(self)
+    self.pubish_static_tf()
+  
+  def publish_static_tf(self):
+    transform = TransformStamped()
+    transform.header.stamp = self.get_clock().now().to_msg()
+    transform.header.frame_id = "base_link"
+    transform.child_frame_id = "camera_link"
+    transform.transform.translation.x = 0.5
+    transform.transform.translation.y = 0.3
+    transform.transform.translation.z = 0.6
+    
+    rotation_quat = quaternion_from_euler(math.radians(180), 0, 0)
+    transform.transform.rotation.x = rotation_quat[0]
+    transform.transform.rotation.y = rotation_quat[1]
+    transform.transform.rotation.z = rotation_quat[2]
+    transform.transform.rotation.w = rotation_quat[3]
+    
+    self.static_broadcaster.sendTransfomr(transform)
+    self.get_logger().info(f'Publish TF: {transform}')
+
+def main():
+  rclpy.init()
+  node = StaticTFBoradcaster()
+  rclpy.super(node)
+  rclpy.shutdown()
