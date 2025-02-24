f33a9cff3953e1bb8d7d28583e8e8dfbe1857b4e
Jason <quietjosen@example.com>
2025-02-24 15:26:41 +0800

    Add launch.

---
+++ chapter4_ws/src/demo_python_service/launch/demo_launch.py
@@ -0,0 +1,19 @@
+import launch
+import launch_ros
+
+def generate_launch_description():
+  action_node_face_detect = launch_ros.actions.Node(
+    package='demo_python_service',
+    executable='face_detect_node',
+    output='log',
+  )
+  action_node_face_detect_client = launch_ros.actions.Node(
+    package='demo_python_service',
+    executable='face_detect_client_node',
+    output='screen',
+  )
+  launch_description = launch.LaunchDescription([
+    action_node_face_detect,
+    action_node_face_detect_client,
+  ])
+  return launch_description
--- chapter4_ws/src/demo_python_service/setup.py
+++ chapter4_ws/src/demo_python_service/setup.py
@@ -7,10 +7,10 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name+"/resource", ['resource/face2.png', 'resource/face3.png']),
+        ('share/' + package_name+"/launch", ['launch/demo_launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
