db65289a84be057ab1dfce0579b95a923bb83984
Jason <quietjosen@example.com>
2025-02-24 15:34:22 +0800

    Add parameter.

--- chapter4_ws/src/demo_python_service/launch/demo_launch.py
+++ chapter4_ws/src/demo_python_service/launch/demo_launch.py
@@ -2,9 +2,11 @@ import launch
 import launch_ros
 
 def generate_launch_description():
+  action_declare_face_locations_model = launch.actions.DeclareLaunchArgument('face_locations_model', default_value='cnn')
   action_node_face_detect = launch_ros.actions.Node(
     package='demo_python_service',
     executable='face_detect_node',
+    parameters=[{'face_locations_model': launch.substitutions.LaunchConfiguration('face_locations_model', default='hog')}],
     output='log',
   )
   action_node_face_detect_client = launch_ros.actions.Node(
@@ -13,6 +15,7 @@ def generate_launch_description():
     output='screen',
   )
   launch_description = launch.LaunchDescription([
+    action_declare_face_locations_model,
     action_node_face_detect,
     action_node_face_detect_client,
   ])
