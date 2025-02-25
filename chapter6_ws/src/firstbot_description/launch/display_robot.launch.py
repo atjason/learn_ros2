02b6c4708451888d5fec51ebc79cb9e138635343
Jason <quietjosen@example.com>
2025-02-25 10:34:37 +0800

    Update fishbot.

--- chapter6_ws/src/firstbot_description/launch/display_robot.launch.py
+++ chapter6_ws/src/firstbot_description/launch/display_robot.launch.py
@@ -4,7 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
   urdf_tutorial_path = get_package_share_directory('firstbot_description')
-  default_model_path = urdf_tutorial_path + '/urdf/first_robot.xacro'
+  default_model_path = urdf_tutorial_path + '/urdf/fishbot/fishbot.urdf.xacro'
   default_rviz_path = urdf_tutorial_path + '/config/display_robot_model.rviz'
 
   action_declare_arg_mode_path = launch.actions.DeclareLaunchArgument(
